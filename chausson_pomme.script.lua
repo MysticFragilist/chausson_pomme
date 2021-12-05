d04d21808ddaf9075029238ff61f48594a7678a8
Samuel Montambault <amos.samuel@hotmail.com>
2021-12-05 14:05:28 -0500

    Create chausson_pomme.script.lua

---
+++ chausson_pomme.script.lua
@@ -0,0 +1,6 @@
+function on.paint(gc)
+    gc:drawString("Chaussons aux pommes", 0,0)
+    gc:drawImage(image.new(_R.IMG.chaussonauxpommesmcdo1), 0, 30)
+end
+
+-- Add as a resource the image in the same folder
